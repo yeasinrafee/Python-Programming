bf6f8eaff97ff151babb9ea427ae5cba62fd298b
yeasinrafee <mdyeasinrafi7@gmail.com>
2022-10-08 13:30:38 +0600

    Slicing the string

---
+++ Day_03/slice.py
@@ -0,0 +1,8 @@
+# sentence = "Controlling complexity is the essence of programming"
+# print(sentence[:11])
+
+# sentence = "Never trust a computer you can't throw out a window"
+# print(sentence[8::3])
+
+sentence = "It's great to work with computers. They don't argue, they remember everything and they don't drink your beer"
+print(sentence[::-1])
