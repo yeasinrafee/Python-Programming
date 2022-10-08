3404d1a3cbd002eb7b90875c6dbfc3491fbe90d4
yeasinrafee <mdyeasinrafi7@gmail.com>
2022-10-08 16:19:47 +0600

    properties of String

---
+++ Day_03/properties.py
@@ -0,0 +1,13 @@
+
+word = "Repetition"
+print(word * 10)
+
+haiku = """Whitecaps on the bay:
+A broken signboard banging
+In the April wind.
+"""
+print(haiku)
+print("broken" in haiku)
+print("sing" not in haiku)
+
+print(len(haiku))
