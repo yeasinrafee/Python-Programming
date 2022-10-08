b12f968a40e6ed8dc0b2cbca60eee98453e9ea07
yeasinrafee <mdyeasinrafi7@gmail.com>
2022-10-08 12:56:02 +0600

    String index() method

---
+++ Day_03/index.py
@@ -0,0 +1,6 @@
+word = "computer"
+print(word[4])
+
+sentence = "In theory, theory and practice are the same. In practice, they are not."
+print(sentence.index("practice"))
+print(sentence.rindex("practice"))
