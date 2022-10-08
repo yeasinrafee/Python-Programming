c068df15f52852a00a209f19612f3a6b66d19444
yeasinrafee <mdyeasinrafi7@gmail.com>
2022-10-08 16:05:38 +0600

    String Method exercise

---
+++ Day_03/methods.py
@@ -0,0 +1,21 @@
+text = "Hello guyazzz, I'm learning string methods"
+# print(text.upper())         # .upper()
+# print(text.lower())         # .lower()
+
+# print(text.find("I"))       # .find()
+
+# print(text.replace("z", "Z"))   # .replace()
+
+textList = text.split()     # .split()
+# print(textList)
+
+a = "My"
+b = "name"
+c = "is"
+d = "Rafee"
+e = " ".join([a, b, c, d])      #.join()
+# print(e)
+
+#Execise:
+sentence = "If the implementation is hard to explain, it might be a bad idea." 
+print(sentence.replace("hard", "easy").replace("bad", "good")) 
