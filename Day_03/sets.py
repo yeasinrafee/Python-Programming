3e64207504037d7f08f45c425c27590beeb3c317
yeasinrafee <mdyeasinrafi7@gmail.com>
2022-10-24 21:05:20 +0600

    List are practiced

---
+++ Day_03/sets.py
@@ -0,0 +1,25 @@
+# my_set = set([2, 3, 4, 7, 7])
+# print(my_set)
+# print(type(my_set))
+#
+# s1 = set((5, 6, 2, (5, 6), 0, 4))
+# print(s1)
+#
+# s2 = {5, 6, 2, 9, 0}
+# # s2.add(3)
+# # s2.remove(0)
+# # s2.pop()
+# # s2.pop()
+# s3 = s2.union(s1)
+# s3.clear()
+# print(s3)
+
+# my_set_1 = {1, 2, 'three', 'four'}
+# my_set_2 = {'three', 'four', 5, 6}
+# my_set_3 = my_set_1.union(my_set_2)
+# print(my_set_3)
+#
+name = {'Yeasin', 'Arafat', 'Rafee', 'Ragner', 'Leonidas'}
+name.pop()
+name.add('Leonidas')
+print(name)
