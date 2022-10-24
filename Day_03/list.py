b243b114b726631fe69a4ff1749b8760df01aaf0
yeasinrafee <mdyeasinrafi7@gmail.com>
2022-10-24 11:24:35 +0600

    list practiced

---
+++ Day_03/list.py
@@ -0,0 +1,12 @@
+# list = ['d', 'a', 'e', 'b', 'c']
+# list.sort()
+# list.reverse()
+# print(list)
+
+# transportation_means  = ['bus', 'truck', 'cycle', 'plane']
+# transportation_means.append('boat')
+# print(transportation_means)
+
+fruits = ['mango', 'banana', 'pineaple', 'watermelon', 'nuts']
+# fruits.pop(2)
+print(fruits[0:2])
