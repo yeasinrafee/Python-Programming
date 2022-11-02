55c79642462a927475facd5dd23e81411950c1b7
yeasinrafee <mdyeasinrafi7@gmail.com>
2022-11-02 11:17:23 +0600

    Comparison Operator

---
+++ Day_04/comparison_oparator.py
@@ -0,0 +1,8 @@
+age = int(input("Enter your age: "))
+
+if age > 18:
+    print("You can drive")
+elif age < 18:
+    print("You can't drive until u became 18")
+else:
+    print("You can drive but you need a license")
